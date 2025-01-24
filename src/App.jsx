1867aebc1ebc1eb101f627ecb6f683908e60defd
mdlpontual <mdlpontual@gmail.com>
2025-01-23 21:21:55 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <div className='container'>
+        <div className='row'>
+          <h1 className='col-3'>col-test</h1>
+          <h1 className='col-3'>col-test</h1>
+          <h1 className='col-1'>yo...</h1>
+          <h2 className='col btn btn-primary'>button-test</h2>
+        </div>
+      </div>  
+    </>
+  )
+}
+
+export default App
