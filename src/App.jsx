5d8022f11d9f207f74a78952a3016abe538e9302
mdlpontual <mdlpontual@gmail.com>
2025-02-17 16:18:12 -0300

    page files

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,7 @@ import IMG from './assets/imagesHUB'
 function App() {
   return (
     <>
-      <div id="aaa" className="container-fluid">
-        <div id="bbb" className="row">
-          <div id="ccc" className="col">
-            <h1>text</h1>
-            <h1>text</h1>
-            <h1>text</h1>
-          </div>
-          <div id="ddd" className="col">
-            <h1>text</h1>
-            <h1>text</h1>
-            <h1>text</h1>
-          </div>
-        </div>
-      </div>
+      
     </>
   )
 }
