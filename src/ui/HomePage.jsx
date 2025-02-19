f2f31d4cd551c71d39bf10ce6cb5f078a6b012aa
mdlpontual <mdlpontual@gmail.com>
2025-02-19 16:46:01 -0300

    setup ok

--- src/ui/HomePage.jsx
+++ src/ui/HomePage.jsx
@@ -3,9 +3,7 @@ import React from "react";
 function HomePage() {
   return (
     <>
-      <div id="test-container" className="container-fluid">
-        <h1>Welcome to the Home Page</h1>
-      </div>
+
     </>
   )
 }
