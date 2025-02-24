a845b04f87934350dedc631f9663096802fe51df
mdlpontual <mdlpontual@gmail.com>
2025-02-24 16:43:18 -0300

    path nfp

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import CasaPaiva from './ui/components/projects/paiva/CasaPaiva.jsx';
 import SeerrambiFlats from './ui/components/projects/serrambi/SerrambiFlats.jsx';
 import Apto404 from './ui/components/projects/404/Apt404.jsx';
 import Aurora1602 from './ui/components/projects/aurora/Aurora1602.jsx';
+import NotFoundPage from './ui/components/NotFoundPage.jsx';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./scss/main.scss";
@@ -16,7 +17,7 @@ const router = createBrowserRouter([
   { path: '/seerrambiFlats', element: <SeerrambiFlats />},
   { path: '/apto404', element: <Apto404 />},
   { path: '/aurora1602', element: <Aurora1602 />},
-  
+  { path: '*', element: <NotFoundPage />}
 ]);
 
 createRoot(document.getElementById('root')).render(
