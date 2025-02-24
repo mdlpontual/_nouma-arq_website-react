a83863ac601adda527c81dd85eb290da08bef04e
mdlpontual <mdlpontual@gmail.com>
2025-02-24 17:40:36 -0300

    start structure prj page

--- src/ui/components/NotFoundPage.jsx
+++ src/ui/components/NotFoundPage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function NotFoundPage() {
     return (
       <>
-        <h1>404 - Page not found</h1>
+        <h1 style={{color: "black"}}>Page not found</h1>
+        <Link to={'/'}><h5>voltar</h5></Link>
       </>
     )
 }
--- src/ui/components/projects/paiva/CasaPaiva.jsx
+++ src/ui/components/projects/paiva/CasaPaiva.jsx
@@ -4,7 +4,30 @@ import IMG from "../../../../assets/imagesHUB";
 function CasaPaiva() {
     return (
       <>
-        <h1 id="test">Casa Paiva</h1>
+        <header>
+            <section id="prjHeaderCon" className="container-fluid" style={{backgroundImage: `url(${IMG.jumbotron_00})`}}>
+                <div id="prjHeaderRow" className="row">
+                    <div id="prjHeaderCol" className="col">
+                        <h1 id="prjTitle">
+                            CASA PAIVA
+                        </h1>
+                    </div>
+                </div>
+            </section>
+        </header>
+        <body>
+            
+        </body>
+        <footer>
+            <section id="prjFooterCon" className="container-fluid">
+                <div id="prjFooterRow" className="row">
+                    <div id="prjFooterCol" className="col-auto">
+                        <h4>NOUMA Arquitetura</h4>
+                        <h5>copyright - 2025</h5>
+                    </div>
+                </div>
+            </section>
+        </footer>
       </>
     )
 }
