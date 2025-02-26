bf9cc744a996ab55e675b68e1d8141c2a5a94748
mdlpontual <mdlpontual@gmail.com>
2025-02-26 20:45:50 -0300

    test 04

--- src/ui/components/jumbotron/Jumbotron.jsx
+++ src/ui/components/jumbotron/Jumbotron.jsx
@@ -1,4 +1,4 @@
-/* import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import IMG from "../../../assets/imagesHUB";
 import Menu from "./menu/Menu";
 
@@ -41,94 +41,6 @@ function Jumbotron() {
   return (
     <>
       <section id="jumboCon" className="container-fluid" style={{backgroundImage: `url(${currentIMG})`}}>
-        <div id={isMenuOpen ? "menuOpen" : "menuClosed"} className="row">
-          {isMenuOpen && <Menu setIsMenuOpen={setIsMenuOpen} />}
-        </div>
-        <div id="jumboFrame" className="row">
-          <div id="jumboInnerFrame" className={`col ${isHovered || isMenuOpen ? "hovered-border" : ""}`}></div>
-          <div id="jumboInnerLogo" className="col">
-            <div id="symbRow" className="row">
-              <img id="nomaSymbol" 
-                   className="col-auto btn" 
-                   src={symbolSrc}
-                   onMouseEnter={handleMouseEnter}
-                   onMouseLeave={handleMouseLeave}
-                   onClick={() => setIsMenuOpen(true)}
-                   alt="nouma symbol and menu button"/>
-            </div>
-            <div id="nameRow" className="row">
-              <img id="nomaName" className="col-auto" src={IMG.nouma_lineName_white} alt="nouma name" />
-            </div>
-          </div>
-        </div>
-      </section>
-    </>
-  )
-}
-
-export default Jumbotron; */
-
-import React, { useState, useEffect } from "react";
-import IMG from "../../../assets/imagesHUB";
-import Menu from "./menu/Menu";
-
-const jumboIMG = [ 
-  IMG.jumbotron_00, IMG.jumbotron_01, IMG.jumbotron_02, IMG.jumbotron_03, IMG.jumbotron_04,
-  IMG.jumbotron_05, IMG.jumbotron_06, IMG.jumbotron_07, IMG.jumbotron_08, IMG.jumbotron_09,
-  IMG.jumbotron_10, IMG.jumbotron_11, IMG.jumbotron_12, IMG.jumbotron_13, IMG.jumbotron_14,
-  IMG.jumbotron_15, IMG.jumbotron_16, IMG.jumbotron_17, 
-  IMG.jumbotron_20, IMG.jumbotron_21, IMG.jumbotron_22, IMG.jumbotron_23, IMG.jumbotron_24,
-  IMG.jumbotron_25, IMG.jumbotron_26, IMG.jumbotron_27, IMG.jumbotron_28, IMG.jumbotron_29,
-];
-
-function Jumbotron() {
-  const [currentIMG, setCurrentIMG] = useState(jumboIMG[Math.floor(Math.random() * jumboIMG.length)]);
-  const [loaded, setLoaded] = useState(false); // New state to track loading
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isHovered, setIsHovered] = useState(false);
-  const [symbolSrc, setSymbolSrc] = useState(IMG.nouma_onlySymbol_white);
-
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-    setTimeout(() => {
-      setSymbolSrc(IMG.nouma_onlySymbol_green);
-    }, 100); 
-  };
-
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-    setTimeout(() => {
-      setSymbolSrc(IMG.nouma_onlySymbol_white);
-    }, 100); 
-  };
-
-  useEffect(() => {
-    const preloadImage = new Image();
-    preloadImage.src = currentIMG;
-    preloadImage.onload = () => setLoaded(true); // Set as loaded when image is ready
-
-    const interval = setInterval(() => {
-      setLoaded(false); // Reset loading state before switching
-      const newImg = jumboIMG[Math.floor(Math.random() * jumboIMG.length)];
-      const img = new Image();
-      img.src = newImg;
-      img.onload = () => {
-        setCurrentIMG(newImg);
-        setLoaded(true);
-      };
-    }, 15000);
-    
-    return () => clearInterval(interval);
-  }, [currentIMG]);
-
-  return (
-    <>
-      <section id="jumboCon" className="container-fluid" 
-                style={{
-                  backgroundImage: `url(${currentIMG})`,
-                  opacity: loaded ? 1 : 0,  // Only show image once it's loaded
-                  transition: "opacity 1s ease-in-out"
-                }}>
         <div id={isMenuOpen ? "menuOpen" : "menuClosed"} className="row">
           {isMenuOpen && <Menu setIsMenuOpen={setIsMenuOpen} />}
         </div>
--- src/ui/components/projects/paiva/CasaPaiva.jsx
+++ src/ui/components/projects/paiva/CasaPaiva.jsx
@@ -1,4 +1,4 @@
-/* import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import IMG from "../../../../assets/imagesHUB";
 import { Link } from "react-router-dom";
 
@@ -64,85 +64,6 @@ function CasaPaiva() {
         </footer>
       </>
     )
-}
-
-export default CasaPaiva;
- */
-
-import React, { useState, useEffect } from "react";
-import IMG from "../../../../assets/imagesHUB";
-import { Link } from "react-router-dom";
-
-const imgArr = [ 
-  IMG.paiva_01, IMG.paiva_02, IMG.paiva_03, IMG.paiva_04, IMG.paiva_05, 
-  IMG.paiva_06, IMG.paiva_07, IMG.paiva_08, IMG.paiva_09, IMG.paiva_10, IMG.paiva_11, 
-  IMG.paiva_12, IMG.paiva_13, IMG.paiva_14, IMG.paiva_15, IMG.paiva_16, IMG.paiva_17, 
-  IMG.paiva_18, IMG.paiva_19, IMG.paiva_20, IMG.paiva_21, IMG.paiva_22
-];
-
-function CasaPaiva() {
-  const [loaded, setLoaded] = useState(false);
-
-  useEffect(() => {
-    const img = new Image();
-    img.src = IMG.jumbotron_00;
-    img.onload = () => setLoaded(true);
-  }, []);
-
-  return (
-    <>
-      <header>
-        <section id="prjHeaderCon" className="container-fluid" style={{
-                backgroundImage: `url(${IMG.jumbotron_00})`,
-                opacity: loaded ? 1 : 0,
-                transition: "opacity 1s ease-in-out"}}>
-            <div id="prjHeaderRow" className="row">
-                <div id="prjHeaderCol" className="col">
-                    <h1 id="prjTitle">
-                        CASA PAIVA
-                    </h1>
-                </div>
-            </div>
-        </section>
-      </header>
-      <body>
-        <section id="preImgsCon" className="container-fluid">
-          <div id="preImgsRow" className="row">
-            <div id="preImgsCol" className="col-auto">
-              <img id="miniTessSymb" src={IMG.tessSymbol_black} alt="simbolo tesserato pequeno" />
-            </div>
-            <div id="prjTextCol" className="col-auto">
-              <h4>Residência na praia do Paiva - Pernambuco</h4>
-              <h4>530m²</h4>
-              <h4>2018-2023</h4>
-            </div>
-          </div>
-        </section>
-        <section id="imgsCon" className="container-fluid">
-          {imgArr.map((img, i) => (
-            <div id="imgsRow" className="row" key={i}>
-              <img id="prjIMG" className="col-auto" src={img} alt="imagem projeto" />
-            </div>
-          ))}
-        </section>
-      </body>
-      <footer>
-        <section id="prjFooterCon" className="container-fluid">
-          <div id="backBtnRow" className="row">
-            <Link to={'/'}>
-              <h4 id="backBtn" className="btn">Página Inicial</h4>
-            </Link>
-          </div>
-          <div id="prjFooterRow" className="row">
-            <div id="prjFooterCol" className="col-auto">
-              <h4>NOUMA Arquitetura</h4>
-              <h5>copyright - 2025</h5>
-            </div>
-          </div>
-        </section>
-      </footer>
-    </>
-  )
 }
 
 export default CasaPaiva;
